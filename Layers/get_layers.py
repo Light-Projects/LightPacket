3968ef19fa00dac18349de8ff466cf58a68a240b
Heretic <adamboulaaz92@gmail.com>
2026-08-15 00:54:56 +0100

    Add files via upload

--- Layers/get_layers.py
+++ Layers/get_layers.py
@@ -3,15 +3,28 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
-def get_all_layers(layer):
-    layers = {}
+def get_all_layers(layer) -> list:
+
+    layers = []
     current = layer
     while current:
-        layers[current.__class__.__name__] = current
-        current = current.payload
+        layers.append(current)
+        try:
+            current = current.payload
+        except AttributeError:
+            break
 
     return layers
 
-def printpc(layers):
+def printpc(packet) -> None:
+    layers = get_all_layers(packet)
     for layer in layers:
         print(layer.__class__.__name__)
+
+
+def haslayer(packet, layer) -> bool:
+    layers = get_all_layers(packet)
+    for i in layers:
+        if i.__class__.__name__ == layer:
+            return True
+    return False
