143c525b879a79f3e8751d18b7ebcd1755349f94
Heretic <adamboulaaz92@gmail.com>
2026-09-05 13:48:35 +0100

    Change version to 0.0.3a in setup.py
    Updated version from 0.0.3w to 0.0.3a in setup.py.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="lightpacket",
-    version="0.0.3w",
+    version="0.0.3a",
     packages=find_packages(),  
     package_data={
         'LightPacket': ['lib/*.so', 'lib/*.dll', 'lib/*.dylib','data/*.txt','helper/protos/*.json'],
